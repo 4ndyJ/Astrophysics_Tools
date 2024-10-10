a4236f71d2cdca0d86a9d33be5288a605ea4ff92
Andrew James <115220449+4ndyJ@users.noreply.github.com>
2024-10-10 17:39:32 +0100

    Update Tools.py
    Added Arcsecond separation function and imports to Tools.
    Converts names of Simbad Queriable objects to separations between them

--- Tools.py
+++ Tools.py
@@ -2,6 +2,8 @@ from astropy.io import fits
 from spaceKLIP import starphot
 import numpy as np
 from astroquery.simbad import Simbad
+import astropy.units as u
+from astropy.coordinates import SkyCoord
 
 
 def KLModeList(idir):
@@ -33,3 +35,33 @@ def Ballesteros(B,V):
     Calculated Temperature (K)
     '''
     return 4600*(1/(0.92*(B-V)+1.7)+1/(0.92*(B-V)+0.62))
+
+
+def arcsecond_separation(Object1, Object2):
+    '''
+    Returns the separation of two objects in arcsecons
+    Inputs
+    Object1 (Str): Simbad Queriable Object
+    Object2 (Str): Simbad Queriable Object
+
+    Returns:
+    separation (float: u.arcsecond): Separation in arcseconds with astropy unit arcsecond
+    '''
+
+    Simbad_Query = Simbad()
+
+    binary1_data = Simbad_Query.query_object(Object1) 
+    ra1 = binary1_data['RA'][0]  # Right Ascension
+    dec1 = binary1_data['DEC'][0]  # Declination
+
+    binary2_data = Simbad_Query.query_object(Object2)  
+    ra2 = binary2_data['RA'][0]  # Right Ascension
+    dec2 = binary2_data['DEC'][0]  # Declination
+
+    # Convert RA and Dec to SkyCoord object 
+    coord1 = SkyCoord(ra=ra1, dec=dec1, unit=(u.hourangle, u.deg))
+    coord2 = SkyCoord(ra=ra2, dec=dec2, unit=(u.hourangle, u.deg))
+
+    # Calculate the separation in arcseconds
+    separation = coord1.separation(coord2).arcsecond
+    return separation
